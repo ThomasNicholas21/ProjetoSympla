689ca766317565fb90ad17484cf2d6d74109a86d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-22 20:18:23 -0300

    feat: criando models: category, batch e events

--- app_sympla/events/models.py
+++ app_sympla/events/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Batch(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Events(models.Model):
+    name = models.CharField(max_length=128)
+    start_date = models.DateTimeField()
+    location = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    sympla_id = models.CharField(max_length=128, unique=True)
+    batch = models.ForeignKey(
+        Batch,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+    )
