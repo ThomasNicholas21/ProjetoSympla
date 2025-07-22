68ed4b579c2b5dfdc47827ce7bbf1cfa9bb325cd
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-22 20:45:50 -0300

    feat: criando model de log

--- app_sympla/logs/models.py
+++ app_sympla/logs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from events.models import Batch
 
-# Create your models here.
+
+class Log(models.Model):
+    batch = models.ForeignKey(
+        Batch,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True
+    )
+    message = models.TextField()
+    imported_amount = models.PositiveIntegerField()
+    status = models.CharField(
+        max_length=128,
+        choices=(
+            ('sucess', 'Sucess'),
+            ('error', 'Error'),
+        )
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
