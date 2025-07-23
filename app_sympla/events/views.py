4195be82f6c398f6fd40d958522b62ca0faa01fe
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-23 20:58:12 -0300

    feat: segunda versão view home

--- app_sympla/events/views.py
+++ app_sympla/events/views.py
@@ -1,68 +1,102 @@
 from django.shortcuts import render
-from events.models import Event, Location, Category, Batch
-from events.services import sympla_service
+from django.db import transaction
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
+from django.utils.timezone import is_naive
+from events.models import Event, Location, Category, Batch
+from events.services import sympla_service
+
+
+URL = "https://api.sympla.com.br/public/v1.5.1/events"
 
 
 def home(request):
     context = {}
 
     if request.method == "POST":
-        s_token = request.POST.get("s_token")
-        url = "https://api.sympla.com.br/public/v3/events"
-
-        if not s_token:
-            s_token = (
-                'a28e68e547aacfa3426548a989e41e39'
-                'f3db9d43a2cfe0ab4a261eec6f240b39'
-            )
+        s_token = request.POST.get("s_token") or (
+            'a28e68e547aacfa3426548a989e41e39'
+            'f3db9d43a2cfe0ab4a261eec6f240b39'
+        )
 
         try:
-            events = sympla_service(url, s_token)
-            batch = Batch.objects.create()
+            events = sympla_service(URL, s_token=s_token)
+            created_count = 0
 
-            for event in events:
-                location_data = event['location']
-                location_obj, _ = Location.objects.get_or_create(
-                    location_name=location_data['location_name'],
-                    city=location_data['city']
-                )
+            with transaction.atomic():
+                batch = Batch.objects.create()
 
-                categories = []
-                event_category = [
-                    event['category_prim'], event['category_sec']
-                ]
-                for category_name in event_category:
-                    if category_name:
-                        category_obj, _ = (
-                            Category.objects.get_or_create(
-                                name=category_name
+                for event in events:
+                    location_data = event.get('location')
+                    location_object, _ = Location.objects.get_or_create(
+                        location_name=location_data['location_name'],
+                        city=location_data['city']
+                    )
+
+                    categories = []
+                    event_categories = [
+                        event['category_prim'], event['category_sec']
+                    ]
+                    for name in event_categories:
+                        if name:
+                            category_object, _ = (
+                                Category.objects.get_or_create(
+                                    name=name
+                                )
                             )
+                            categories.append(category_object)
+
+                    start_date = parse_datetime(event['start_date'])
+                    if is_naive(start_date):
+                        start_date = make_aware(start_date)
+
+                    last_event = Event.objects.filter(
+                        sympla_id=event['sympla_id']).order_by(
+                            '-batch__id'
+                        ).first()
+                    should_create = False
+
+                    if not last_event:
+                        should_create = True
+                    else:
+                        if (
+                            last_event.name != event['name'] or
+                            last_event.start_date != start_date or
+                            last_event.location != location_object or
+                            set(
+                                last_event.category.values_list(
+                                    'name', flat=True
+                                    )
+                                ) != set(
+                                    [
+                                        c.name for c in categories
+                                    ]
+                                )
+                        ):
+                            should_create = True
+
+                    if should_create:
+                        event_object = Event.objects.create(
+                            sympla_id=event['sympla_id'],
+                            name=event['name'],
+                            start_date=start_date,
+                            location=location_object,
+                            batch=batch
                         )
-                        categories.append(category_obj)
+                        event_object.category.set(categories)
+                        created_count += 1
 
-                start_date = make_aware(parse_datetime(event['start_date']))
-
-                event_obj = Event.objects.create(
-                    name=event['name'],
-                    start_date=start_date,
-                    location=location_obj,
-                    sympla_id=event['sympla_id'] + event['start_date'],
-                    batch=batch
-                )
-
-                event_obj.category.set(categories)
-
-
-            context['success'] = f"{len(events)} eventos importados com sucesso!" # noqa
+            if created_count:
+                context['success'] = f"{created_count} evento(s) importado(s)!"
+            else:
+                context['success'] = "Nenhum evento novo para importar."
 
         except Exception as e:
-            context['error'] = f"Ocorreu um erro: {e}"
+            context['error'] = f"Ocorreu um erro: {str(e)}"
 
     return render(request, "events/page/sympla.html", context)
 
 
 def list_view(request):
-    batches = Batch.objects.prefetch_related('event_set__location', 'event_set__category') # noqa
+    batches = Batch.objects.prefetch_related('event_set__location', 'event_set__category').order_by('-id') # noqa
     return render(request, 'events/page/list_sympla.html', {'batches': batches}) # noqa
