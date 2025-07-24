673cba7538382c886613d663558345973d911872
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-24 19:16:43 -0300

    refactor: refatorando services para modularizar

--- app_sympla/events/services.py
+++ app_sympla/events/services.py
@@ -9,39 +9,31 @@ def sympla_service(
             page: int
         ) -> list[dict]:
     headers: dict[str] = {'s_token': s_token}
+    paginated_url: str = f'{url}?page={page}&page_size={page_size}'
 
     try:
-        paginated_url: str = f'{url}?page={page}&page_size={page_size}'
         response = requests.get(paginated_url, headers=headers, timeout=10)
-
         response.raise_for_status()
+        response_json = response.json()
 
-        try:
-            response_json = response.json()
+    except requests.exceptions.Timeout:
+        raise TimeoutError(
+            "A requisição expirou."
+        )
 
-        except requests.exceptions.Timeout:
-            raise TimeoutError(
-                "A requisição expirou."
-            )
-
-        except requests.exceptions.HTTPError as e:
-            raise ConnectionError(
-                f"Erro HTTP: {e.response.reason}"
-            )
-
-        except requests.exceptions.RequestException as e:
-            raise ConnectionError(
-                f"Erro de conexão com a API Sympla: {str(e)}"
-            )
-
-        except ValueError:
-            raise ValueError(
-                "Erro ao fazer parse do Json"
-            )
+    except requests.exceptions.HTTPError as e:
+        raise ConnectionError(
+            f"Erro HTTP: {e.response.reason}"
+        )
 
     except requests.exceptions.RequestException as e:
-        raise requests.exceptions.RequestException(
-            f'Erro na requisição: {e}'
+        raise ConnectionError(
+            f"Erro de conexão com a API Sympla: {str(e)}"
+        )
+
+    except ValueError:
+        raise ValueError(
+            "Erro ao fazer parse do Json"
         )
 
     data_event: list = [
@@ -59,27 +51,29 @@ def normalize_data(data: dict) -> dict[str | datetime]:
     location: dict[str] = {
         'location_name': (
             str(
-                'Evento Online' if
-                address.get('address_num') == '0'
-                else
-                address.get('name', 'Local não informado')
+                'Evento Online'
+                if address.get('address_num') == '0'
+                else address.get('name', 'Local não informado')
                 ).strip().title()
         ),
         'city': (
             str(
-                None if
-                address.get('address_num') == '0'
-                else
-                address.get('city', 'Cidade não informada')
+                'Evento Online'
+                if address.get('address_num') == '0'
+                else address.get('city', 'Cidade não informada')
                 ).strip().title()
         )
     }
 
     category_prim: str = (
-        str(data.get('category_prim', {}).get('name', '')).title()
+        str(
+            data.get('category_prim', {}).get('name', '')
+            ).strip().title()
     )
     category_sec: str = (
-        str(data.get('category_sec', {}).get('name', '')).title()
+        str(
+            data.get('category_sec', {}).get('name', '')
+            ).strip().title()
     )
 
     sympla_id: str = (
