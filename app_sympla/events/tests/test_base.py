c1edab0668785826bd69f69b89e88e4425df3111
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-24 19:17:01 -0300

    tests: arrumando criação de eventos

--- app_sympla/events/tests/test_base.py
+++ app_sympla/events/tests/test_base.py
@@ -7,17 +7,27 @@ class EventsFixture(TestCase):
     def setUp(self):
         return super().setUp()
 
+    def make_location(
+            self,
+            location_name='Test-Location',
+            city='Test-City'
+            ):
+        return models.Location.objects.create(
+            location_name=location_name,
+            city=city
+        )
+
     def make_category(self, name='Category Test'):
         return models.Category.objects.create(name=name)
 
     def make_batch(self):
         return models.Batch.objects.create()
 
-    def create_event(
+    def make_event(
         self,
         name='Test',
         start_date=None,
-        location='Name Test + Location Test',
+        location=None,
         category=None,
         sympla_id='dasdg',
         batch=None,
@@ -25,17 +35,22 @@ class EventsFixture(TestCase):
         if start_date is None:
             start_date = make_aware(datetime(2025, 12, 31, 12, 30))
 
+        if location is None:
+            location = self.make_location()
+
         if category is None:
             category = self.make_category()
 
         if batch is None:
             batch = self.make_batch()
 
-        return models.Events.objects.create(
+        event = models.Event.objects.create(
             name=name,
             start_date=start_date,
             location=location,
-            category=category,
             sympla_id=sympla_id,
             batch=batch,
         )
+        event.category.set([category])
+
+        return event
