d4472b0fe4de5259f95d09f31e03eb310ff14f2d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-24 17:57:42 -0300

    refactor: modularizando service

--- app_sympla/events/services.py
+++ app_sympla/events/services.py
@@ -1,18 +1,39 @@
 import requests
-from pprint import pprint
+from django.utils.timezone import datetime
 
 
-def sympla_service(url: str, s_token: str) -> list[dict]:
-    headers = {'s_token': s_token}
-    data_event = []
+def sympla_service(
+            url: str,
+            s_token: str,
+            page_size: int,
+            page: int
+        ) -> list[dict]:
+    headers: dict[str] = {'s_token': s_token}
 
     try:
-        response = requests.get(url, headers=headers, timeout=10)
+        paginated_url: str = f'{url}?page={page}&page_size={page_size}'
+        response = requests.get(paginated_url, headers=headers, timeout=10)
 
         response.raise_for_status()
 
         try:
             response_json = response.json()
+
+        except requests.exceptions.Timeout:
+            raise TimeoutError(
+                "A requisição expirou."
+            )
+
+        except requests.exceptions.HTTPError as e:
+            raise ConnectionError(
+                f"Erro HTTP: {e.response.reason}"
+            )
+
+        except requests.exceptions.RequestException as e:
+            raise ConnectionError(
+                f"Erro de conexão com a API Sympla: {str(e)}"
+            )
+
         except ValueError:
             raise ValueError(
                 "Erro ao fazer parse do Json"
@@ -23,64 +44,53 @@ def sympla_service(url: str, s_token: str) -> list[dict]:
             f'Erro na requisição: {e}'
         )
 
-    for data in response_json.get('data', []):
-        try:
-            event_name = str(data.get('name', '')).strip().title()
-            start_date = data.get('start_date')
-
-            address = data.get('address', {})
-            location = {
-                'location_name': (
-                    str(
-                        'Evento Online' if
-                        address.get('address_num') == '0'
-                        else
-                        address.get('name', 'Local não informado')
-                        ).strip().title()
-                ),
-                'city': (
-                    str(
-                        'Evento Online' if
-                        address.get('address_num') == '0'
-                        else
-                        address.get('city', 'Cidade não informada')
-                        ).strip().title()
-                )
-            }
-
-            category_prim = (
-                str(data.get('category_prim', {}).get('name', '')).title()
-            )
-            category_sec = (
-                str(data.get('category_sec', {}).get('name', '')).title()
-            )
-
-            sympla_id = (
-                str(data.get('id')).strip().lower()
-            )
-
-            data_event.append(
-                {
-                    'name': event_name,
-                    'start_date': start_date,
-                    'location': location,
-                    'category_prim': category_prim,
-                    'category_sec': category_sec,
-                    'sympla_id': sympla_id
-                }
-            )
-
-        except Exception as e:
-            raise Exception(
-                f'Erro ao processar: {e}'
-            )
+    data_event: list = [
+        normalize_data(data) for data in response_json.get('data', [])
+    ]
 
     return data_event
 
 
-if __name__ == "__main__":
-    url = 'https://api.sympla.com.br/public/v1.5.1/events'
-    s_token = 'a28e68e547aacfa3426548a989e41e39f3db9d43a2cfe0ab4a261eec6f240b39' # noqa
-    data_event = sympla_service(url, s_token) # noqa
-    for event in data_event:
-        pprint(event)
+def normalize_data(data: dict) -> dict[str | datetime]:
+    event_name: str = str(data.get('name', '')).strip().title()
+    start_date: datetime = data.get('start_date')
+
+    address: dict = data.get('address', {})
+    location: dict[str] = {
+        'location_name': (
+            str(
+                'Evento Online' if
+                address.get('address_num') == '0'
+                else
+                address.get('name', 'Local não informado')
+                ).strip().title()
+        ),
+        'city': (
+            str(
+                None if
+                address.get('address_num') == '0'
+                else
+                address.get('city', 'Cidade não informada')
+                ).strip().title()
+        )
+    }
+
+    category_prim: str = (
+        str(data.get('category_prim', {}).get('name', '')).title()
+    )
+    category_sec: str = (
+        str(data.get('category_sec', {}).get('name', '')).title()
+    )
+
+    sympla_id: str = (
+        str(data.get('id')).strip().lower()
+    )
+
+    return {
+        "name": event_name,
+        "start_date": start_date,
+        "location": location,
+        "category_prim": category_prim,
+        "category_sec": category_sec,
+        "sympla_id": sympla_id
+    }
