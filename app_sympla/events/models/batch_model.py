12f20604af88acf86f72a4b4a748e38e014cb969
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-23 22:28:38 -0300

    feat: criando manager para batch

--- app_sympla/events/models/batch_model.py
+++ app_sympla/events/models/batch_model.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 
+class BatchManager(models.Manager):
+    def prefetch_location_and_category_set(self):
+        return self.prefetch_related(
+            'event_set__location', 'event_set__category'
+        )
+
+
 class Batch(models.Model):
+    objects = BatchManager()
     created_at = models.DateTimeField(auto_now_add=True)
