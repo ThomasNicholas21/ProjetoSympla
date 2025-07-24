54f35ac27aca0d5d000817039579f35914bde823
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-23 22:31:44 -0300

    chore: arrumando importação

--- app_sympla/events/models/__init__.py
+++ app_sympla/events/models/__init__.py
@@ -1,4 +1,4 @@
 from .batch_model import Batch # noqa
 from .category_model import Category # noqa
-from .event_model import Events # noqa
+from .event_model import Event # noqa
 from .location_model import Location # noqa
