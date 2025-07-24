33ccb6b9a912b1662903b95d76507043eb55e2be
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-24 18:32:18 -0300

    test: connection error

--- app_sympla/events/tests/test_service.py
+++ app_sympla/events/tests/test_service.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from unittest.mock import patch
-from requests.exceptions import Timeout
+from requests.exceptions import Timeout, HTTPError
 from events.services import sympla_service
+import requests
 
 
 class TestService(TestCase):
@@ -10,3 +11,14 @@ class TestService(TestCase):
             with self.assertRaises(TimeoutError) as ctx:
                 sympla_service("http://fakeurl.com", "fake_token", 100, 1)
             self.assertIn("a requisição expirou.", str(ctx.exception).lower())
+
+    def test_http_error_raises_connection_error(self):
+        response = requests.Response()
+        response.status_code = 500
+        response.reason = "Internal Server Error"
+        http_error = HTTPError(response=response)
+
+        with patch("requests.get", side_effect=http_error):
+            with self.assertRaises(ConnectionError) as ctx:
+                sympla_service("http://fakeurl.com", "fake_token", 100, 1)
+            self.assertIn("erro http:", str(ctx.exception).lower())
