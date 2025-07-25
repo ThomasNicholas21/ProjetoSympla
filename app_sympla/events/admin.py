7683d05e092b977182583c08ce8f85f26fce9467
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-25 11:18:14 -0300

    feat: configurando ambiente admin

--- app_sympla/events/admin.py
+++ app_sympla/events/admin.py
@@ -1,17 +1,33 @@
 from django.contrib import admin
-from events.models import Category, Batch, Event
+from events.models import Category, Batch, Event, Location
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_per_page = 5
+    list_max_show_all = 10
+    ordering = '-id',
 
 
 @admin.register(Batch)
 class BatchAdmin(admin.ModelAdmin):
-    pass
+    list_per_page = 5
+    list_max_show_all = 10
+    list_display = ['id', 'created_at']
+    ordering = '-id',
 
 
 @admin.register(Event)
 class EventsAdmin(admin.ModelAdmin):
-    pass
+    list_per_page = 5
+    list_max_show_all = 10
+    list_display = ['id', 'name', 'start_date']
+    ordering = '-id',
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_per_page = 5
+    list_max_show_all = 10
+    list_display = ['id', 'location_name', 'city']
+    ordering = '-id',
--- app_sympla/logs/admin.py
+++ app_sympla/logs/admin.py
@@ -4,4 +4,10 @@ from logs.models import Log
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
-    pass
+    list_per_page = 5
+    list_max_show_all = 10
+    list_display = [
+        'id', 'batch', 'created_at',
+        'imported_amount',
+    ]
+    ordering = '-id',
